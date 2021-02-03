383baf87d070ad7c131bbc26c8b4c2c20d597fd1
grajkogj <67935829+grajkogj@users.noreply.github.com>
2021-02-03 12:57:27 -0600

    Add files via upload

---
+++ ClientBase/Modules/MembeanAC.js
@@ -0,0 +1,9 @@
+class MembeanAC extends module {
+        constructor() {
+                super(module)
+                this.ModuleName = "Membean Hacks";
+        }
+        execute() {
+                console.log("this works")
+        }
+};
