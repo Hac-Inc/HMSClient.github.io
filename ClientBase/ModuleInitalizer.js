13d1691d8d9c97a181602dfdaf13ce1501d334a3
grajkogj <67935829+grajkogj@users.noreply.github.com>
2021-02-03 16:29:05 -0600

    Add files via upload

--- ClientBase/ModuleInitalizer.js
+++ ClientBase/ModuleInitalizer.js
@@ -2,10 +2,12 @@ function initModules() {
         
         //Create your modules here:
         let MembeanAC_Module = new MembeanAC;
+        let TabFreeze_Module = new TabFreeze;
 
 
         //add your modules to this list:
         modules.push(MembeanAC_Module);
+        modules.push(TabFreeze_Module);
 
 }
 setTimeout(initModules(), 500)
