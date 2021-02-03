113377eff9b80e662a9a299ffa34b4157ce26752
grajkogj <67935829+grajkogj@users.noreply.github.com>
2021-02-03 15:40:12 -0600

    Add files via upload

--- ClientBase/BuildTools.js
+++ ClientBase/BuildTools.js
@@ -7,12 +7,16 @@ function newDiv(id) {
         div.style.borderWidth = "1px";
         return div;
 }
-function newButton(id, DisplayName, onclick) {
+function newButton(id, DisplayName, moduleListId) {
         let button = document.createElement('input');
         button.type = "button";
         button.id = id;
         button.value = DisplayName;
         button.style.padding = "5px";
-        button.onclick = onclick;
+        setTimeout(
+                function() {
+                        document.getElementById(id).addEventListener('click', function() {modules[moduleListId].execute()})
+                }, 100
+        )
         return button;
 }
