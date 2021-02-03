50543c3ce2ab26039025dbad09fddc23935fde58
grajkogj <67935829+grajkogj@users.noreply.github.com>
2021-02-03 13:47:26 -0600

    Add files via upload

--- ClientBase/BuildTools.js
+++ ClientBase/BuildTools.js
@@ -1,1 +1,9 @@
-//not empty anymore
+function newButton(id, DisplayName, onclick) {
+        button = document.createElement('input');
+        button.type = "button";
+        button.id = id;
+        button.value = DisplayName;
+        button.style.padding = "5px";
+        button.onclick = onclick;
+        return button;
+}
--- ClientBase/ModuleManager.js
+++ ClientBase/ModuleManager.js
@@ -1,5 +1,13 @@
 class module {
         constructor() {
                 this.ModuleName;
+                this.ModuleId;
+        }
+        getModuleName() {
+                return this.ModuleName;
+        }
+        getModuleId() {
+                return this.ModuleId;
         }
 }
+
