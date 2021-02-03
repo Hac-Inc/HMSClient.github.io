62566ed4dfc0a82cbfc8c179a25d3105515c2b69
grajkogj <67935829+grajkogj@users.noreply.github.com>
2021-02-03 14:28:23 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-//not empty
+function buildHMSClient() {
+        let x = document.createElement('div');
+        x.id = "HMSClientWindow"
+        x.style.padding = "5px;";
+        x.style.background = "#525252";
+        x.style.border = "solid";
+        x.style.borderWidth = "1px";
+        x.style.position = "absolute";
+        x.style.zIndex = "999999";
+        document.body.appendChild(x);
+        document.getElementById("HMSClientWindow").appendChild(newDiv("HMSClientWindow-header"))
+        for(i=0;i<=modules.length;i++) {
+                document.getElementById("HMSClientWindow").appendChild(newButton(modules[i].getModuleId(), modules[i].getModuleName(), modules[i].execute()))
+        }
+}
