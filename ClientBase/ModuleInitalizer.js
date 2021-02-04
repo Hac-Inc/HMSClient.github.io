b46a5132924775a899e167762b6705d563f3075d
grajkogj <67935829+grajkogj@users.noreply.github.com>
2021-02-04 01:57:39 -0600

    Add files via upload

--- ClientBase/ModuleInitalizer.js
+++ ClientBase/ModuleInitalizer.js
@@ -1,14 +1,21 @@
 function initModules() {
-        
-        //Create your modules here:
-        let MembeanAC_Module = new MembeanAC;
-        let TabFreeze_Module = new TabFreeze;
+        try {
 
 
-        //add your modules to this list:
-        modules.push(MembeanAC_Module);
-        modules.push(TabFreeze_Module);
+                //Create your modules here:
+                let MembeanAC_Module = new MembeanAC;
+                let TabFreeze_Module = new TabFreeze;
+
+                //Add them to the compiler list:
+                modules.push(MembeanAC_Module);
+                modules.push(TabFreeze_Module);
+
+
+        } catch (error) {
+                alert("Injection Failed. Please refresh your page and inject again.");
+        }
 
 }
-setTimeout(initModules(), 1000)
-setTimeout(buildHMSClient(), 1500)
+
+setTimeout(initModules(), 1000);
+setTimeout(buildHMSClient(), 1001);
