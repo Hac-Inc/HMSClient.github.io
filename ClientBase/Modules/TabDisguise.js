0404cf45970d67cc4188174f8fda79f8eff31fc1
grajkogj <67935829+grajkogj@users.noreply.github.com>
2021-02-04 13:15:34 -0600

    Add files via upload

---
+++ ClientBase/Modules/TabDisguise.js
@@ -0,0 +1,23 @@
+class TabDisguise extends module {
+        constructor() {
+                super(module);
+                this.ModuleId = "TabDisguise";
+                this.ModuleName = "Tab Disguiser";
+                this.category = "misc";
+        }
+        execute() {
+                let f = document.getElementsByTagName('link')
+                for(i=0;i<f.length;i++) {
+                        if(f[i].rel == "shortcut icon") {
+                                f[i].remove();
+                        }
+                }
+                let x = document.createElement('link');
+                x.rel = "shortcut icon";
+                x.type = "image/png";
+                x.href = "https://hac-inc.github.io/HMSClient.github.io/img/driveIcon.png";
+                document.head.appendChild(x);
+                document.title = "My Drive - Google Drive";
+        }
+
+}
