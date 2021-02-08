ab581e264a24d64939085ee16bc49095afbb3c7e
Obes7777 <78672497+Obes7777@users.noreply.github.com>
2021-02-08 14:36:18 -0600

    Add files via upload

---
+++ ClientBase/Modules/test.js
@@ -0,0 +1,12 @@
+class test extends module {
+    constructor () {
+        super(module);
+        this.moduleId = "test";
+        this.ModuleName = "test";
+        this.category = "misc";
+    
+    }
+    execute () {
+        system.out.println("hi");
+    }
+}
