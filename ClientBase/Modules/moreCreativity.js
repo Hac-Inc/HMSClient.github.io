1ce2003a796b63e4b696df8bd5b0208696eef2ed
Obes7777 <78672497+Obes7777@users.noreply.github.com>
2021-02-08 21:15:52 -0600

    Add files via upload

---
+++ ClientBase/Modules/moreCreativity.js
@@ -0,0 +1,14 @@
+class moreCreativity extends module {
+    constructor () {
+        super(module);
+        this.moduleId = "moreCreativity";
+        this.ModuleName = "more creativity";
+        this.category = "paperclips";
+    
+    }
+    execute () {
+        processors += 1000;
+        creativity += 1000;
+        memory += 1000;
+    }
+}
