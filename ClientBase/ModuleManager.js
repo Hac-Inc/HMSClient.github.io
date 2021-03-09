992cd2ee5f90dc3eea24a5b3a66dbcc94706cfff
Slap <67935829+grajkogj@users.noreply.github.com>
2021-03-09 04:09:21 -0600

    Add files via upload

--- ClientBase/ModuleManager.js
+++ ClientBase/ModuleManager.js
@@ -2,6 +2,71 @@ let modules = [];
 
 
 class module {
+
+        registerBoolSetting(tiedModuleId, displayName, varName, defaultValue) {
+                let br = document.createElement("br");
+                let setting = document.createElement("input");
+                let label = document.createElement('label');
+                label.for = varName + "-boolSetting";
+                label.innerHTML = displayName;
+                label.style.fontFamily = "arial";
+                setting.type = "checkbox";
+                setting.id = varName + "-boolSetting";
+                setting.name = displayName;
+                if(defaultValue == true) {
+                        setting.checked = true;
+                } else {
+                        setting.checked = false;
+                };
+                setTimeout(function() {
+                        if(document.getElementById(tiedModuleId + "-moduleS-setting-container").hasChildNodes()) {
+                                document.getElementById(tiedModuleId + "-moduleS-setting-container").appendChild(br);
+                        }
+                        document.getElementById(tiedModuleId + "-moduleS-setting-container").appendChild(setting);
+                        document.getElementById(tiedModuleId + "-moduleS-setting-container").appendChild(label);
+                },1010);
+        };
+        
+        boolSetting(setting) {
+                if(document.getElementById(setting + "-boolSetting").checked == true) {
+                        return true;
+                } else if(document.getElementById(setting + "-boolSetting").checked == false) {
+                        return false;
+                }
+        }
+        
+        
+        registerIntSetting(tiedModuleId, displayName, varName, defaultValue, minValue, maxValue) {
+                let br = document.createElement("br");
+                let setting = document.createElement("input");
+                let label = document.createElement('label');
+                label.for = varName + "-intSetting";
+                label.innerHTML = displayName + ": " + defaultValue;
+                label.style.fontFamily = "arial";
+                label.id = varName + "-intSetting-label";
+                setting.type = "range";
+                setting.id = varName + "-intSetting";
+                setting.name = displayName;
+                setting.value = defaultValue;
+                setting.min = minValue;
+                setting.max = maxValue;
+                setTimeout(function() {
+                        if(document.getElementById(tiedModuleId + "-moduleS-setting-container").hasChildNodes()) {
+                                document.getElementById(tiedModuleId + "-moduleS-setting-container").appendChild(br);
+                        }
+                        document.getElementById(tiedModuleId + "-moduleS-setting-container").appendChild(setting);
+                        document.getElementById(tiedModuleId + "-moduleS-setting-container").appendChild(label);
+                        document.getElementById(varName + "-intSetting").addEventListener('change', function() {
+                                document.getElementById(varName + "-intSetting-label").innerHTML = displayName + ": " + document.getElementById(varName + "-intSetting").value;
+                        })
+                },1010);
+        }
+        
+        intSetting(setting) {
+                let value = document.getElementById(setting + "-intSetting").value;
+                return value;
+        }
+
         constructor() {
                 this.ModuleName;
                 this.ModuleId;
