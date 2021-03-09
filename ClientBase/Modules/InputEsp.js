431c929c214395d38179ae9038fb2b264df0e7d1
Slap <67935829+grajkogj@users.noreply.github.com>
2021-03-09 02:41:36 -0600

    refactor for toggleability and settings

--- ClientBase/Modules/InputEsp.js
+++ ClientBase/Modules/InputEsp.js
@@ -5,39 +5,116 @@ class InputEsp extends module {
                 this.ModuleId = "InputEsp";
                 this.category = "page";
                 this.tooltip = "Highlights all inputs on the page with the chosen color."
+                this.toggleable = true;
+                this.hasSettings = true;
+
+                registerBoolSetting(this.ModuleId, "Inputs", "esp_inputs", true);
+                registerBoolSetting(this.ModuleId, "Buttons", "esp_buttons", true);
+                registerBoolSetting(this.ModuleId, "Textareas", "esp_textarea", true);
+                registerBoolSetting(this.ModuleId, "Links", "esp_links", true);
+                registerBoolSetting(this.ModuleId, "Selects", "esp_selects", true);
+
                 this.color;
         }
-        execute() {
+
+        whileActive() {
+                console.log("this does indeed work")
+        }
+        
+        onActivate() {
                 this.color = prompt("Please type the color for the ESP: ");
-                let x = document.getElementsByTagName("input");
-                for(i=0;i<x.length;i++) {
-                        x[i].style.border = "solid";
-                        x[i].style.borderColor = this.color;
-                        x[i].style.borderWidth = "5px";
+                if(boolSetting("esp_inputs")) {
+                        let x = document.getElementsByTagName("input");
+                        for(i=0;i<x.length;i++) {
+                                x[i].style.border = "solid";
+                                x[i].style.borderColor = this.color;
+                                x[i].style.borderWidth = "5px";
+                        }
                 }
-                let b = document.getElementsByTagName("button");
-                for(i=0;i<b.length;i++) {
-                        b[i].style.border = "solid";
-                        b[i].style.borderColor = this.color;
-                        b[i].style.borderWidth = "5px";
+                
+                if(boolSetting("esp_buttons")) {
+                        let b = document.getElementsByTagName("button");
+                        for(i=0;i<b.length;i++) {
+                                b[i].style.border = "solid";
+                                b[i].style.borderColor = this.color;
+                                b[i].style.borderWidth = "5px";
+                        }
                 }
-                let t = document.getElementsByTagName("textarea");
-                for(i=0;i<t.length;i++) {
-                        t[i].style.border = "solid";
-                        t[i].style.borderColor = this.color;
-                        t[i].style.borderWidth = "5px";
+                
+                if(boolSetting("esp_textareas")) {
+                        let t = document.getElementsByTagName("textarea");
+                        for(i=0;i<t.length;i++) {
+                                t[i].style.border = "solid";
+                                t[i].style.borderColor = this.color;
+                                t[i].style.borderWidth = "5px";
+                        }
                 }
-                let a = document.getElementsByTagName("a");
-                for(i=0;i<a.length;i++) {
-                        a[i].style.border = "solid";
-                        a[i].style.borderColor = this.color;
-                        a[i].style.borderWidth = "5px";
+                
+                if(boolSetting("esp_links")) {
+                        let a = document.getElementsByTagName("a");
+                        for(i=0;i<a.length;i++) {
+                                a[i].style.border = "solid";
+                                a[i].style.borderColor = this.color;
+                                a[i].style.borderWidth = "5px";
+                        }
                 }
-                let s = document.getElementsByTagName("select");
-                for(i=0;i<s.length;i++) {
-                        s[i].style.border = "solid";
-                        s[i].style.borderColor = this.color;
-                        s[i].style.borderWidth = "5px";
+                
+                if(boolSetting("esp_selects")) {
+                        let s = document.getElementsByTagName("select");
+                        for(i=0;i<s.length;i++) {
+                                s[i].style.border = "solid";
+                                s[i].style.borderColor = this.color;
+                                s[i].style.borderWidth = "5px";
+                        }
+                }
+                
+        }
+
+        onDeactivate() {
+                if(boolSetting("esp_inputs")) {
+                        let x = document.getElementsByTagName("input");
+                        for(i=0;i<x.length;i++) {
+                                x[i].style.border = "none";
+                                x[i].style.borderColor = this.color;
+                                x[i].style.borderWidth = "5px";
+                        }
+                }
+                
+                if(boolSetting("esp_buttons")) {
+                        let b = document.getElementsByTagName("button");
+                        for(i=0;i<b.length;i++) {
+                                b[i].style.border = "none";
+                                b[i].style.borderColor = this.color;
+                                b[i].style.borderWidth = "5px";
+                        }
+                }
+                
+                if(boolSetting("esp_textareas")) {
+                        let t = document.getElementsByTagName("textarea");
+                        for(i=0;i<t.length;i++) {
+                                t[i].style.border = "none";
+                                t[i].style.borderColor = this.color;
+                                t[i].style.borderWidth = "5px";
+                        }
+                }
+                
+                if(boolSetting("esp_links")) {
+                        let a = document.getElementsByTagName("a");
+                        for(i=0;i<a.length;i++) {
+                                a[i].style.border = "none";
+                                a[i].style.borderColor = this.color;
+                                a[i].style.borderWidth = "5px";
+                        }
+                }
+                
+                if(boolSetting("esp_selects")) {
+                        let s = document.getElementsByTagName("select");
+                        for(i=0;i<s.length;i++) {
+                                s[i].style.border = "none";
+                                s[i].style.borderColor = this.color;
+                                s[i].style.borderWidth = "5px";
+                        }
                 }
         }
+
 }
