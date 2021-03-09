02dd4caf46921d5c70a6ae23b399d12e56d77a7d
Slap <67935829+grajkogj@users.noreply.github.com>
2021-03-09 01:41:17 -0600

    big update

--- index.js
+++ index.js
@@ -141,9 +141,14 @@ function buildHMSClientScreen() {
         }
         //insertes all the modules in their designated subcategory:
         for(i=0;i<modules.length;i++) {
-                document.getElementById(modules[i].getModuleCategory() + "-subCategoryS").appendChild(newButton((modules[i].getModuleId() + "-moduleS"), (modules[i].getModuleName()), i, modules[i].getModuleTooltip(), "#a3a3a3", "#6e6e6e"));
-                let br = document.createElement('br');
-                document.getElementById(modules[i].getModuleCategory() + "-subCategoryS").appendChild(br);
+                let d = document.createElement('div');
+                d.id = modules[i].getModuleId() + "-moduleS-container";
+                let s = document.createElement('div');
+                s.id = modules[i].getModuleId() + "-moduleS-setting-container";
+                s.style.display = "none";
+                document.getElementById(modules[i].getModuleCategory() + "-subCategoryS").appendChild(d);
+                document.getElementById(modules[i].getModuleId() + "-moduleS-container").appendChild(newButton((modules[i].getModuleId() + "-moduleS"), (modules[i].getModuleName()), i, modules[i].getModuleTooltip(), "#a3a3a3", "#6e6e6e"));
+                document.getElementById(modules[i].getModuleId() + "-moduleS-container").appendChild(s);
         }
 
         document.body.addEventListener('keypress', e => {
