0819d072bf76101bf7ef0329883fa8cac68fa312
Slap <67935829+grajkogj@users.noreply.github.com>
2021-03-09 12:01:44 -0600

    Add files via upload

--- ClientBase/ModuleManager.js
+++ ClientBase/ModuleManager.js
@@ -1,26 +1,13 @@
 let modules = [];
 
+let m_settings = [];
+
 
 class module {
 
         registerBoolSetting(tiedModuleId, displayName, varName, defaultValue) {
-                let br = document.createElement("br");
-                let setting = document.createElement("input");
-                let label = document.createElement('label');
-                label.for = varName + "-boolSetting";
-                label.innerHTML = displayName;
-                label.style.fontFamily = "arial";
-                setting.type = "checkbox";
-                setting.id = varName + "-boolSetting";
-                setting.name = displayName;
-                if(defaultValue == true) {
-                        setting.checked = true;
-                } else {
-                        setting.checked = false;
-                };
-                setTimeout(function() {
-                        drawModuleBoolSetting(tiedModuleId, setting, label);
-                },2010);
+                let setting = new m_boolSetting(tiedModuleId, displayName, varName, defaultValue);
+                m_settings.push(setting);
         };
         
         boolSetting(setting) {
@@ -33,22 +20,8 @@ class module {
         
         
         registerIntSetting(tiedModuleId, displayName, varName, defaultValue, minValue, maxValue) {
-                let br = document.createElement("br");
-                let setting = document.createElement("input");
-                let label = document.createElement('label');
-                label.for = varName + "-intSetting";
-                label.innerHTML = displayName + ": " + defaultValue;
-                label.style.fontFamily = "arial";
-                label.id = varName + "-intSetting-label";
-                setting.type = "range";
-                setting.id = varName + "-intSetting";
-                setting.name = displayName;
-                setting.value = defaultValue;
-                setting.min = minValue;
-                setting.max = maxValue;
-                setTimeout(function() {
-                        drawModuleIntSetting(tiedModuleId, setting, label, displayName, varName)
-                },2010);
+                let setting = new m_intSetting(tiedModuleId, displayName, varName, defaultValue, minValue, maxValue);
+                m_settings.push(setting);
         }
         
         intSetting(setting) {
@@ -124,6 +97,81 @@ class module {
         
 
 }
+
+
+
+
+class m_boolSetting {
+        constructor(tiedModuleId, displayName, varName, defaultValue) {
+                this.tiedModuleId = tiedModuleId;
+                this.displayName = displayName;
+                this.varName = varName;
+                this.defaultValue = defaultValue;
+        }
+        draw() {
+                let br = document.createElement("br");
+                let setting = document.createElement("input");
+                let label = document.createElement('label');
+                label.for = this.varName + "-boolSetting";
+                label.innerHTML = this.displayName;
+                label.style.fontFamily = "arial";
+                setting.type = "checkbox";
+                setting.id = this.varName + "-boolSetting";
+                setting.name = this.displayName;
+                if(this.defaultValue == true) {
+                        setting.checked = true;
+                } else {
+                        setting.checked = false;
+                };
+                setTimeout(function() {
+                        if(document.getElementById(this.tiedModuleId + "-moduleS-setting-container").hasChildNodes()) {
+                                document.getElementById(this.tiedModuleId + "-moduleS-setting-container").appendChild(br);
+                        }
+                        document.getElementById(this.tiedModuleId + "-moduleS-setting-container").appendChild(setting);
+                        document.getElementById(this.tiedModuleId + "-moduleS-setting-container").appendChild(label);
+                },2010);
+        }
+}
+
+class m_intSetting {
+        constructor(tiedModuleId, displayName, varName, defaultValue, minValue, maxValue) {
+                this.tiedModuleId = tiedModuleId;
+                this.displayName = displayName;
+                this.varName = varName;
+                this.defaultValue = defaultValue;
+                this.minValue = minValue;
+                this.maxValue = maxValue;
+        }
+        draw() {
+                let br = document.createElement("br");
+                let setting = document.createElement("input");
+                let label = document.createElement('label');
+                label.for = this.varName + "-intSetting";
+                label.innerHTML = this.displayName + ": " + this.defaultValue;
+                label.style.fontFamily = "arial";
+                label.id = this.varName + "-intSetting-label";
+                setting.type = "range";
+                setting.id = this.varName + "-intSetting";
+                setting.name = this.displayName;
+                setting.value = this.defaultValue;
+                setting.min = this.minValue;
+                setting.max = this.maxValue;
+                setTimeout(function() {
+                        if(document.getElementById(this.tiedModuleId + "-moduleS-setting-container").hasChildNodes()) {
+                                document.getElementById(this.tiedModuleId + "-moduleS-setting-container").appendChild(br);
+                        }
+                        document.getElementById(this.tiedModuleId + "-moduleS-setting-container").appendChild(setting);
+                        document.getElementById(this.tiedModuleId + "-moduleS-setting-container").appendChild(label);
+                        document.getElementById(this.varName + "-intSetting").addEventListener('change', function() {
+                                document.getElementById(this.varName + "-intSetting-label").innerHTML = this.displayName + ": " + document.getElementById(this.varName + "-intSetting").value;
+                        })
+                },2010);
+        }
+}
+
+
+
+
 /*
 function registerBoolSetting(tiedModuleId, displayName, varName, defaultValue) {
         let br = document.createElement("br");
