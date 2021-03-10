6c732a330b53b0c96774a960e6a16beb5db00773
Slap <67935829+grajkogj@users.noreply.github.com>
2021-03-10 03:22:30 -0600

    add array list module

---
+++ ClientBase/Modules/HudArrayList.js
@@ -0,0 +1,33 @@
+class HudArrayList extends module {
+        constructor() {
+                super(module);
+                this.ModuleId = "HudArrayList";
+                this.ModuleName = "Array List";
+                this.category = "hud";
+                this.tooltip = "Toggles the active modules array list";
+                this.toggleable = true;
+        }
+
+        onActivate() {
+                let x = document.createElement('div');
+                x.style.position = "absolute";
+                x.style.top = "0px";
+                x.style.right = "0px";
+                x.style.zIndex = "9999999"
+                x.style.display = "grid";
+                x.id = "HMSClientModuleArrayList";
+                document.body.appendChild(x);
+                for(i=0;i<modules.length;i++) {
+                        if(modules[i].active == true && modules[i].ModuleId != this.ModuleId) {
+                                drawArrayListModule(modules[i].ModuleId, modules[i].ModuleName);
+                        }
+                }
+        }
+        onDeactivate() {
+                document.getElementById("HMSClientModuleArrayList")?.remove();
+        }
+        whileActive() {
+                document.getElementById("HMSClientModuleArrayList").style.top = document.documentElement.scrollTop + "px";
+                document.getElementById("HMSClientModuleArrayList").style.right = document.documentElement.scrollRight + "px";
+        }
+}
